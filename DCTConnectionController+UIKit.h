4fcc1c8419f683b7d77e28c190ffb36a2e1a560e
Daniel Tull <dt@danieltull.co.uk>
2011-11-11 23:28:23 +0000

    Add dependency on DCTConnectionController

--- DCTConnectionController+UIKit.h
+++ DCTConnectionController+UIKit.h
@@ -7,8 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "DCTConnectionController.h"
 
 #ifndef dctconnectioncontroller_uikit
 #define dctconnectioncontroller_uikit_1_0     10000
 #define dctconnectioncontroller_uikit         dctconnectioncontroller_uikit_1_0
+#endif
+
+#if !defined(dctconnectioncontroller) || !defined(dctconnectioncontroller_2_0) || dctconnectioncontroller < dctconnectioncontroller_2_0
+#warning "DCTConnectionController 2.1 is required with this version of DCTConnectionController+UIKit. Update at https://github.com/danielctull/DCTConnectionController"
 #endif
